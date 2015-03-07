fc5d42752cde7ce8721c8d32d48925167f0310a4
Stephane de Labrusse <stephdl@de-labrusse.fr>
2015-03-07 15:27:17 +0100

    * Sat Mar 7 2015 stephane de labrusse <stephdl@de-labrusse.fr> 1.0.0-2.ns6 - url is present in nethgui

---
+++ root/usr/share/nethesis/NethServer/Module/Dashboard/Applications/tt-rss.php
@@ -0,0 +1,26 @@
+<?php
+namespace NethServer\Module\Dashboard\Applications;
+
+/**
+ * tt-rss web interface
+ *
+ * @author stephane de labrusse
+ */
+class tt-rss extends \Nethgui\Module\AbstractModule implements \NethServer\Module\Dashboard\Interfaces\ApplicationInterface
+{
+
+    public function getName()
+    {
+        return "tt-rss web interface";
+    }
+
+    public function getInfo()
+    {
+         $host = explode(':',$_SERVER['HTTP_HOST']);
+         return array(
+            'url' => "https://".$host[0]."/tt-rss/"
+         );
+    }
+}
+
+
